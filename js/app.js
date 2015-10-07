b76c0821daecea84e64c3a29d66cfec3dedf7959
Casey Runnells <crunnells@gmail.com>
2015-10-06 17:08:00 -0700

    updated html file

--- js/app.js
+++ js/app.js
@@ -15,3 +15,59 @@ $(document).ready(function(){
 });
 
 
+(function(){
+    
+    function answerFeedBack(delta){
+        
+        if (delta > 20){
+     		return "Cold!";
+     	}
+      	if (delta <= 20 && delta > 10){
+  
+     		console.log('warm!')
+     	}        
+      	if (delta <= 10){
+  
+     		console.log('hot!')
+     	}
+    }
+
+    function mainGame(){
+        var ask = document.getElementById("userGuess").value;
+    	var rightAnswer = Math.random()*100;
+		var delta = Math.abs(rightAnswer - ask);
+		var numGuesses = 0;
+        
+        var guessesAllowed = 5;
+        
+        while(delta != 0 || numGuesses === guessesAllowed){
+            numGuesses += 1;
+        	var ask = prompt("Take a Guess");            
+			delta = Math.abs(rightAnswer - ask);    
+            document.getElementById("#answerContainer").textContent = answerFeedBack(delta);
+        }
+        
+        if (delta === 0 && numGuesses < guessesAllowed){
+            alert("Congratulations! You won a new car! It took you " + numGuesses + " guesses, so you win nothing!");
+   			}
+        
+        
+    function startNewGame(){
+
+    }
+
+   mainGame();
+
+
+
+
+
+
+
+var button = document.getElementById("guessButton"),
+value =  button.form.userGuess.value;
+button.onclick = function() {
+    guess(value);
+}
+
+})();
